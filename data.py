543c969458ad8da0db48e2804f3cf75c6f6f3203
Bartek Siudeja <siudej@gmail.com>
2016-03-20 21:42:04 -0700

    improved two cluster data

--- data.py
+++ data.py
@@ -2,7 +2,8 @@
 
 from numba import jit
 import numpy as np
-import numexpr as ne
+# import numexpr as ne
+from scipy.spatial.distance import cdist
 
 
 @jit("void(f8[:,:], f8, f8)", nopython=True, nogil=True)
@@ -20,45 +21,18 @@ def symmetric_gen(A, sigma, sep):
             A[i, j + n] = A[j + n, i] = np.random.normal(sep, sigma)
 
 
-def onedimensionpair(k, l, sigma):
+def two_clusters(k, l, sep, dim=2):
     """
-    Retuern squared distances for two clusters from normal distribution.
+    Return squared distances for two clusters from normal distribution.
 
     k, l - sizes of clusters,
-    sigma>0 - distance between clusters.
+    sep>0 - distance between clusters.
     """
-    X = np.random.normal(size=(k, 1))
-    Y = np.random.normal(size=(l, 1)) + 2 / sigma
-    Z = np.concatenate((X, Y))
-    # print X
-    # print Y
-    dist = ne.evaluate("(Z - ZT)**2", global_dict={'ZT': Z.transpose()})
-    # for i in range(n):
-    #    for j in range(n):
-    #         dist[i, j] = (Z[i] - Z[j]) * (Z[i] - Z[j])
-    dist = sigma * dist
-    return dist
+    Z = np.random.normal(size=(k+l, dim))
+    Z[k:, 0] += sep
+    Z = Z[Z[:, 0].argsort()]
+    return cdist(Z, Z, 'sqeuclidean'), Z
 
-
-def twodimensionpair(k,l,sigma):  #k,l are sizes of points.  sigma is about how far away points in the same cluster are.  Sigma must be positive. Also returns point set for later display
-	X = np.random.normal(size = (k,2))
-	Y = np.random.normal(size = (l,2))+ [2/sigma,0]
-	Z = np.concatenate((X,Y))
-	#print X
-	#print Y
-	#print Z
-	print type(Z)
-	print Z
-	n = len(Z)
-	R = Z[Z[:,0].argsort()]
-	Z = R
-	print Z
-	dist = np.zeros((n, n))
-	for i in range(n):
-		for j in range(n):
-			dist[i,j]=(Z[i,0]-Z[j,0])*(Z[i,0]-Z[j,0])+(Z[i,1]-Z[j,1])*(Z[i,1]-Z[j,1])
-	dist = sigma*dist
-	return dist,Z
 
 def cyclegraph(n, noise):
     """
@@ -92,7 +66,7 @@ def closefarsimplices(n, noise, separation):
 def tests(size='small'):
     """ Generate a few data sets for testing. """
     if size == 'small':
-        return [onedimensionpair(3, 2, 0.1), cyclegraph(5, 0.1),
+        return [two_clusters(3, 2, 0.1, 1)[1], cyclegraph(5, 0.1),
                 closefarsimplices(3, 0.1, 5)]
     else:
         return [closefarsimplices(50, 0.1, 5), closefarsimplices(100, 0.1, 5)]
